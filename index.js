f816f7a5866f3f5ba7c849167617a814845f4e22
Jake Crump <jake.crump@washpost.com>
2018-11-20 23:33:44 -0500

    quick fix for some last minute file name changes to simplify things

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 
 
-const csvFilePath='./output.csv'
+const csvFilePath='./input.csv'
 const csv=require('csvtojson');
 
 let spaceBetweenLines = 15;
@@ -11,13 +11,13 @@ let spaceBetweenLines = 15;
 csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
-    fs.readFile('./test.svg', 'utf-8', (err, bod)=>{
+    fs.readFile('./initial.svg', 'utf-8', (err, bod)=>{
     	const $ = cheerio.load(bod);
     	for (let i in jsonObj){
     		$('svg').append(`<text transform="matrix(1 0 0 1 58.7788 ${spaceBetweenLines})" class="st1 st2">${jsonObj[i].something}</text>`)
     		spaceBetweenLines += 15;
     	}	
-    	fs.writeFileSync('./output.svg', $.html(), 'utf-8')
+    	fs.writeFileSync('./output.svg', $.html('svg'), 'utf-8')
     	console.log('converted!!')
     })
     /**
